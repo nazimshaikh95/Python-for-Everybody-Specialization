3a5fb4b4ea32703c99331c020c2fe361305711cb
Nazim Shaikh <nazimsk9595@gmail.com>
2020-06-15 08:08:18 +0530

    Add files via upload

---
+++ 4 Using Databases With Python/week 5/geodata/where.js
@@ -0,0 +1,15 @@
+myData = [
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.018923,31.499674, 'AUC Avenue، Road، First New Cairo, Cairo Governorate 11835, Egypt'],
+[19.0759837,72.8776559, 'Mumbai, Maharashtra, India'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[39.3301047,-82.1076022, 'Athens, OH 45701, USA'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[6.8939569,3.7187158, 'Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9504351,77.5021998, 'Main Rd, Gnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[31.5469132,-97.1210998, '1311 S 5th St, Waco, TX 76706, USA'],
+[39.9619537,116.3662615, '19 Xinjiekou Outer St, Bei Tai Ping Zhuang, Haidian Qu, Beijing Shi, China, 100875'],
+[53.8938988,27.5460609, 'praspiekt Niezaliežnasci 4, Minsk, Belarus']
+];
